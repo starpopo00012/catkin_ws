e70be8dce55ff2c0d4b339e91664f6a9b6620741
M.JITTAWUT <starpopo00012@gmail.com>
2023-08-24 17:31:42 +0700

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/por/ros_workshop_ws/devel/lib:/home/por/tutorial_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PWD='/home/por/catkin_ws/build'
